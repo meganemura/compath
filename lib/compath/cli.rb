16bd7a2c1601ed902212c9013ae3a59d2866db68
meganemura <meganemura@users.noreply.github.com>
2017-06-11 03:09:42 +0900

    Load .compath.yml only if exists

--- lib/compath/cli.rb
+++ lib/compath/cli.rb
@@ -4,11 +4,12 @@ module Compath
       finder = Compath::Finder.new
       paths = Compath::PathSorter.sort(finder.paths.keys)
 
-      config = Compath::ConfigLoader.load(File.read('.compath.yml'))
-
       guides = paths.map {|path| Compath::Guide.new(path) }
       guide_book = Compath::GuideBook.new(guides)
-      guide_book.merge(config)
+      if FileTest.exist?('.compath.yml')
+        config = Compath::ConfigLoader.load(File.read('.compath.yml'))
+        guide_book.merge(config)
+      end
 
       yaml = guide_book.publish_yaml
 
