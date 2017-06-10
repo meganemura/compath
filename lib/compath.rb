a92acc406a28178be85a1b0d4e7bf8a415643f3f
meganemura <meganemura@users.noreply.github.com>
2017-06-10 13:33:35 +0900

    bundle gem compath --exe --mit --test=rspec

---
+++ lib/compath.rb
@@ -0,0 +1,5 @@
+require "compath/version"
+
+module Compath
+  # Your code goes here...
+end
---
+++ spec/compath_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Compath do
+  it "has a version number" do
+    expect(Compath::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
