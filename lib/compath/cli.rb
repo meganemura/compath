55f7a45aa48488e1e7d7eaaf75970b0e0472f677
meganemura <meganemura@users.noreply.github.com>
2017-06-11 23:00:20 +0900

    Publish top level guides if generating is first time

--- lib/compath/cli.rb
+++ lib/compath/cli.rb
@@ -7,9 +7,15 @@ module Compath
       guides = paths.map {|path| Compath::Guide.new(path) }
       guides.select!(&:directory?)
       guide_book = Compath::GuideBook.new(guides)
+
       if FileTest.exist?('.compath.yml')
         config = Compath::ConfigLoader.load(File.read('.compath.yml'))
         guide_book.merge(config)
+      else
+        initial_config_guides = paths.map {|path| Compath::Guide.new(path, scan: false) }
+        initial_config_guides.select!(&:directory?)
+        initial_config = Compath::GuideBook.new(initial_config_guides)
+        guide_book.merge(initial_config.guides)
       end
 
       yaml = guide_book.publish_yaml
