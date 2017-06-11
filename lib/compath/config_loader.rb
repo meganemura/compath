543c348bf84ef71b2e3ffcbe8658a39c353054d3
meganemura <meganemura@users.noreply.github.com>
2017-06-11 21:14:26 +0900

    Change yaml format for publishing (no backward compatibility)

--- lib/compath/config_loader.rb
+++ lib/compath/config_loader.rb
@@ -4,12 +4,13 @@ module Compath
     def load(config)
       conf = YAML.load(config)
 
-      guides = conf.map do |guide_hash|
-        path = guide_hash.keys.first
-        options = guide_hash.values.first
-        options = options.each_with_object({}) do |(key, value), hash|
+      guides = conf.map do |path, guide|
+        # symbolize keys
+        options = guide.each_with_object({}) do |(key, value), hash|
           hash[key.to_sym] = value
         end
+
+        # TODO: Bad interface...
         Guide.new(path, **options)
       end
     end
