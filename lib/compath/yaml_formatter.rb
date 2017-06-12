b54397573395b4a955a30057571d98d226723773
meganemura <meganemura@users.noreply.github.com>
2017-06-12 14:14:29 +0900

    Improve YamlFormatter

--- lib/compath/yaml_formatter.rb
+++ lib/compath/yaml_formatter.rb
@@ -14,12 +14,30 @@ module Compath
     end
 
     def formatters
-      @formatters ||= [remove_trailing_space_formatter]
+      @formatters ||= [
+        remove_trailing_space_formatter,
+        remove_quotes_from_keys_formatter,
+      ]
     end
 
     def remove_trailing_space_formatter
       lambda do |yaml|
-        yaml.lines.map(&:rstrip).join("\n")
+        yaml.lines.map(&:rstrip).map {|x| "#{x}\n" }.join
+      end
+    end
+
+    QUOTED_KEY_REGEXP = /^"(?<key>[^"]+)":(?<value>.*)/
+    def remove_quotes_from_keys_formatter
+      lambda do |yaml|
+        lines = yaml.lines.map do |line|
+          if m = QUOTED_KEY_REGEXP.match(line)
+            "#{m[:key]}: #{m[:value]}"
+          else
+            line.chomp
+          end
+        end
+
+        lines.map {|x| "#{x}\n" }.join
       end
     end
   end
